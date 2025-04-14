508c6266c968ae794e3b84f039b7c7169e0ecddf
aakashsaini09 <aakashsaini948585@gmail.com>
2025-04-14 14:19:33 +0530

    css design

---
+++ app/blog/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div className='w-full bg-black text-white py-[30vh]'>
+        <div className='box h-[30vh] w-full border border-orange-400 flex justify-center items-center blurClass'>This is Box 1</div>
+        <div className='box h-[30vh] w-full border border-orange-400 flex justify-center items-center blurClass'>This is Box 2</div>
+        <div className='box h-[30vh] w-full border border-orange-400 flex justify-center items-center blurClass'>This is Box 3</div>
+        <div className='box h-[30vh] w-full border border-orange-400 flex justify-center items-center blurClass'>This is Box 5</div>
+        <div className='box h-[30vh] w-full border border-orange-400 flex justify-center items-center blurClass'>This is Box 6</div>
+        <div className='box h-[30vh] w-full border border-orange-400 flex justify-center items-center blurClass'>This is Box 7</div>
+      </div>
+    </>
+  )
+}
+
+export default page
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Yaldevi } from "next/font/google";
-import Image from "next/image";
 import { FaYoutube } from "react-icons/fa";
 export default function Home() {
   return (
@@ -40,14 +38,14 @@ export default function Home() {
   </div>
 
   </div>
-  <img src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905b9f809b5c8180ce30c5_pattern-1.svg" alt="" className="absolute top-16 left-0 right-auto bottom-auto -z-10 w-24 sm:w-32 md:bottom-1/2 md:left-0 md:right-auto md:top-auto" />
-  <img src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905ba1538296b3f50a905e_pattern-2.svg" alt="" className="absolute bottom-auto left-auto right-0 top-0 -z-10 hidden sm:inline-block" />
+  <img src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905b9f809b5c8180ce30c5_pattern-1.svg" alt="" className="img absolute top-16 left-0 right-auto bottom-auto -z-10 w-24 sm:w-32 md:bottom-1/2 md:left-0 md:right-auto md:top-auto" />
+  <img src="https://assets.website-files.com/63904f663019b0d8edf8d57c/63905ba1538296b3f50a905e_pattern-2.svg" alt="" className="img absolute bottom-auto left-auto right-0 top-0 -z-10 hidden sm:inline-block" />
 </section>
 <section>
 <div className="pt-5 bg-gray-100 dark:bg-gray-900" id="pricing" data-theme="light">
   <div className="mx-auto pb-20 mt-4 max-w-7xl px-6 lg:px-8">
     <div className="mx-auto max-w-4xl text-center">
-      <h1 className="text-base font-semibold leading-7 text-indigo-600 dark:text-indigo-400">Pricing</h1>
+      <h1 className="autoshot text-base font-semibold leading-7 text-indigo-600 dark:text-indigo-400">Pricing</h1>
       <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
         Whether it's just you, or your entire team - we've got you covered.
       </p>
@@ -57,7 +55,7 @@ export default function Home() {
     </p>
     <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
-    <div className="ring-1 ring-gray-300 dark:ring-white/10 rounded-3xl p-8 xl:p-10">
+    <div className="autoshot ring-1 ring-gray-300 dark:ring-white/10 rounded-3xl p-8 xl:p-10">
         <div className="flex items-center justify-between gap-x-4">
           <h2 id="product1" className="text-lg font-semibold leading-8 text-gray-900 dark:text-white">
             Product Type 1
@@ -126,7 +124,7 @@ export default function Home() {
         </ul>
       </div>
 
-      <div className="bg-white/5 ring-2 ring-indigo-700 rounded-3xl p-8 xl:p-10 shadow-indigo-700 shadow-2xl">
+      <div className="autoshot bg-white/5 ring-2 ring-indigo-700 rounded-3xl p-8 xl:p-10 shadow-indigo-700 shadow-2xl">
         <div className="flex items-baseline justify-between gap-x-4">
           <h2 id="product2" className="text-lg font-semibold leading-8 text-black dark:text-white">Product Type 2</h2>
           <p className="rounded-full bg-indigo-700 px-2.5 py-1 text-xs font-semibold leading-5 text-white">Most popular</p>
@@ -160,7 +158,7 @@ export default function Home() {
         </ul>
       </div>
 
-      <div className="ring-1 ring-gray-300 dark:ring-white/10 rounded-3xl p-8 xl:p-10">
+      <div className="autoshot ring-1 ring-gray-300 dark:ring-white/10 rounded-3xl p-8 xl:p-10">
         <div className="flex items-center justify-between gap-x-4">
           <h2 id="product1" className="text-lg font-semibold leading-8 text-gray-900 dark:text-white">
             Product Type 3

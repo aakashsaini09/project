112f50e4ce5770dac8c77369954e96bf13d8624c
aakashsaini09 <aakashsaini948585@gmail.com>
2025-04-19 12:37:31 +0530

    scrollTrigger

--- app/Simple/script.js
+++ app/Simple/script.js
@@ -1,12 +1,18 @@
-gsap.from("#p1 .box", {
+gsap.from("#p1 #box", {
     scale: 0,
     delay:1,
     duration:2,
     rotate: 360
 })
-gsap.from("#p2 .box", {
+gsap.from("#p2 #box", {
     scale: 0,
     // delay:1,
     duration:2,
-    rotate: 360
+    rotate: 360,
+    // scrollTrigger:"#p2 #box"
+    scrollTrigger:{
+        trigger: "#p2 #box",
+        scroller: "body",
+        markers: true
+    }
 })

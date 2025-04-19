c4ba8a249526cae22191d64dcf61f59f69061f9d
aakashsaini09 <aakashsaini948585@gmail.com>
2025-04-19 11:55:43 +0530

    GSAP

---
+++ app/Simple/script.js
@@ -0,0 +1,12 @@
+gsap.from("#p1 .box", {
+    scale: 0,
+    delay:1,
+    duration:2,
+    rotate: 360
+})
+gsap.from("#p2 .box", {
+    scale: 0,
+    // delay:1,
+    duration:2,
+    rotate: 360
+})

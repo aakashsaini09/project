44f598cb0f0a0d0cbefa8f85c2462dae5483a205
aakashsaini09 <aakashsaini948585@gmail.com>
2025-04-21 11:58:33 +0530

    gsap timeline

--- app/Simple/script.js
+++ app/Simple/script.js
@@ -58,65 +58,94 @@
 //     }
 // })
 
-gsap.to("#p2 h1", {
-    transform:"translateX(-150%)",
-    scrollTrigger:{
-        trigger: "#p2",
-        scroller:"body",
-        // markers: true,
-        start:"top 0%",
-        end: "top -150%",
-        // toggleActions: "restart none none none",
-        pin: true,
-        scrub:2
-    }
+// gsap.to("#p2 h1", {
+//     transform:"translateX(-150%)",
+//     scrollTrigger:{
+//         trigger: "#p2",
+//         scroller:"body",
+//         // markers: true,
+//         start:"top 0%",
+//         end: "top -150%",
+//         // toggleActions: "restart none none none",
+//         pin: true,
+//         scrub:2
+//     }
+// })
+
+// var initialPath = `M 10 100 Q 500 100 990 100`
+// var finalPath = `M 10 100 Q 500 100 990 100`
+
+// var string  = document.querySelector("#string")
+// string.addEventListener("mousemove", function(dets){
+//     path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
+//     gsap.to("svg path", {
+//         attr: { d: path },
+//         duration: 0.3,
+//         duration: "power3.out"
+//     })
+// })
+// string.addEventListener("mouseleave", function(){
+//     gsap.to("svg path",  {
+//         attr: { d:  finalPath },
+//         duration: 1.5,
+//         ease: "elastic.out(1, 0.2)"
+//     })
+// })
+// var main = document.querySelector("#main")
+// var cursor = document.querySelector("#cursor")
+// var img = document.querySelector("#img")
+
+// main.addEventListener("mousemove", function(event){
+
+//     gsap.to(cursor, {
+//         x: event.x,
+//         y: event.y,
+//         duration: 0.5,
+//         // ease:"bounce.out"
+//     })
+// })
+
+// img.addEventListener("mouseenter", function(event){
+//     cursor.innerHTML = "View More"
+//     gsap.to(cursor,{
+//         scale: 4,
+//         backgroundColor: "#ffffff3a"
+//     })
+// })
+// img.addEventListener("mouseleave", function(event){
+//     cursor.innerHTML = ""
+//     gsap.to(cursor,{
+//         scale: 1,
+//         backgroundColor: "white"
+//     })
+// })
+
+var menu = document.querySelector("#bnav i")
+var cross = document.querySelector("#full i")
+
+var tl = gsap.timeline()
+
+tl.to("#full", {
+    right: 0,
+    duration: 0.5
 })
 
-var initialPath = `M 10 100 Q 500 100 990 100`
-var finalPath = `M 10 100 Q 500 100 990 100`
-
-var string  = document.querySelector("#string")
-string.addEventListener("mousemove", function(dets){
-    path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
-    gsap.to("svg path", {
-        attr: { d: path },
-        duration: 0.3,
-        duration: "power3.out"
-    })
-})
-string.addEventListener("mouseleave", function(){
-    gsap.to("svg path",  {
-        attr: { d:  finalPath },
-        duration: 1.5,
-        ease: "elastic.out(1, 0.2)"
-    })
-})
-var main = document.querySelector("#main")
-var cursor = document.querySelector("#cursor")
-var img = document.querySelector("#img")
-
-main.addEventListener("mousemove", function(event){
-
-    gsap.to(cursor, {
-        x: event.x,
-        y: event.y,
-        duration: 0.5,
-        // ease:"bounce.out"
-    })
+tl.from("#full h4", {
+    x: 150,
+    duration: 0.6,
+    stagger: 0.2,
+    opacity: 0
 })
 
-img.addEventListener("mouseenter", function(event){
-    cursor.innerHTML = "View More"
-    gsap.to(cursor,{
-        scale: 4,
-        backgroundColor: "#ffffff3a"
-    })
-})
-img.addEventListener("mouseleave", function(event){
-    cursor.innerHTML = ""
-    gsap.to(cursor,{
-        scale: 1,
-        backgroundColor: "white"
-    })
+tl.from("#full i",{
+    opacity: 0
 })
 
+tl.pause()
+menu.addEventListener("click", function(){
+    tl.play()
+})
+
+cross.addEventListener("click", function(){
+    tl.reverse()
+})
